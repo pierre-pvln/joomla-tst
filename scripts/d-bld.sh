32639778cc12cfb7b41bf905a8064ac50bd808d3
Pierre <pierre@pvln.nl>
2017-08-10 18:23:14 +0000

    bld and hst changed

--- scripts/d-bld.sh
+++ scripts/d-bld.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# set excecute mode for shell scripts 
+chmod +x *.sh
+
 # reads and executes commands from filename in the current shell environment
 source set_bld.sh
 source set_apache2.sh
 source set_mysql.sh
 
+env
+
 # use environment variables during build
-docker build --tag $my_build_name \ 
-             --build-arg my_apache2_servername=$servername \ 
-             --build-arg my_apache2_sitename=$websitename \
-			 --build-arg my_mysql-server_root_password=$mysql_root_pw \
-			 ../
+docker build --tag $my_build_name --build-arg my_apache2_servername=$servername --build-arg my_apache2_sitename=$websitename --build-arg my_mysql-server_root_password=$mysql_root_pw ../
+
--- scripts/d-hst.sh
+++ scripts/d-hst.sh
@@ -3,8 +3,4 @@
 # reads and executes commands from filename in the current shell environment
 source set_bld.sh
 
-echo "==========="
-echo "Buildname: " $my_build_name
-echo "==========="
-
 docker history --human --no-trunc $my_build_name
